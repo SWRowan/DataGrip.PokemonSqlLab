1cc915babccd7692ead6d84c7cd338735b5c0618
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-03-21 09:38:20 -0400

    Completed lab

--- pokemon_sql/Part3.sql
+++ pokemon_sql/Part3.sql
@@ -1,7 +1,47 @@
 #PART3
 #What is each pokemon's primary type?
-SELECT types.name
-FROM pokemon.types,
-     pokemon.pokemons
-where primary_type = types.id;
+SELECT p.name, t.name
+FROM pokemon.types t,
+     pokemon.pokemons p
+WHERE primary_type = t.id;
+
+#What is Rufflet's secondary type?
+SELECT p.name, t.NAME
+FROM pokemon.types t
+JOIN pokemon.pokemons p ON primary_type = t.id
+WHERE p.name = 'Rufflet';
+
+#What are the names of the pokemon that belong to the trainer with trainerID 303?
+SELECT p.name
+FROM Pokemon.pokemon_trainer pt
+JOIN Pokemon.pokemons p ON p.id = pt.pokemon_id
+JOIN Pokemon.trainers t ON t.trainerID = pt.trainerID
+WHERE t.trainerID = 303;
+
+#How many pokemon have a secondary type Poison
+SELECT t.name , count(t.name) AS NumOfPokemon
+FROM Pokemon.types t
+JOIN Pokemon.pokemons p ON primary_type = t.id
+WHERE t.name = 'Poison';
+
+#How many pokemon at level 100 does each trainer with at least one level 100
+#pokemone have? (Hint: your query should not display a trainer
+SELECT trainerID, count(*) AS Lvl100Pokemon
+FROM Pokemon.pokemon_trainer pt
+WHERE pt.pokelevel = 100
+GROUP BY trainerID;
+
+#How many pokemon only belong to one trainer and no other?
+SELECT count(pokemon_id) AS NumOfPokemon
+FROM (
+     SELECT pt.pokemon_id
+     FROM Pokemon.pokemon_trainer pt
+     GROUP BY pt.pokemon_id
+     HAVING count(*) = 1 ) AS pokemon;
+
+
+
+
+
+
 
---
+++ pokemon_sql/Part4.sql
@@ -0,0 +1,21 @@
+#Final Report
+
+SELECT p.name AS PokemonName, t.trainername AS Trainer, pt.pokelevel AS PokeLevel, ptype.name AS PrimaryType, stype.name AS SecondaryType
+FROM Pokemon.pokemons p
+JOIN Pokemon.pokemon_trainer pt ON p.id = pokemon_id
+JOIN Pokemon.trainers t ON pt.trainerID = t.trainerID
+JOIN Pokemon.types ptype ON ptype.id = p.primary_type
+JOIN Pokemon.types stype ON stype.id = p.secondary_type
+ORDER BY pt.pokelevel DESC ;
+
+
+
+
+#Primary type is Steel and Pokelevel 100
+SELECT p.name AS Name, pt.pokelevel AS Level, ptype.name AS PrimaryType, stype.name AS Secondary
+FROM Pokemon.pokemons p
+JOIN Pokemon.pokemon_trainer pt ON p.id = pokemon_id
+JOIN Pokemon.types ptype ON ptype.id = p.primary_type
+JOIN Pokemon.types stype ON stype.id = p.secondary_type
+WHERE pt.pokelevel = 100 AND ptype.id = 11
+ORDER BY p.name;
