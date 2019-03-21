41ab0f10ac7049ddba542fef2ca1d362f6c11c9c
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-03-21 17:09:26 -0400

    Completed Lab

--- pokemon_sql/Part4.sql
+++ pokemon_sql/Part4.sql
@@ -12,9 +12,10 @@ ORDER BY pt.pokelevel DESC ;
 
 
 #Primary type is Steel and Pokelevel 100
-SELECT p.name AS Name, pt.pokelevel AS Level, ptype.name AS PrimaryType, stype.name AS Secondary
+SELECT p.name AS Name, pt.pokelevel AS Level, ptype.name AS PrimaryType, stype.name AS Secondary, t.trainername AS Trainer
 FROM Pokemon.pokemons p
 JOIN Pokemon.pokemon_trainer pt ON p.id = pokemon_id
+JOIN Pokemon.trainers t ON pt.trainerID = t.trainerID
 JOIN Pokemon.types ptype ON ptype.id = p.primary_type
 JOIN Pokemon.types stype ON stype.id = p.secondary_type
 WHERE pt.pokelevel = 100 AND ptype.id = 11
